b23aeb4f704210ff7842d3d8a171d05b81b83941
mikhail.prisheltsev <mikhail.prisheltsev@gmail.com>
2020-11-03 09:56:47 +0300

    Added pagination component

---
+++ force-app/main/default/lwc/simplePagination/simplePagination.js
@@ -0,0 +1,81 @@
+import { LightningElement, track, api } from "lwc";
+
+const columns = [
+  { label: "Symbols", fieldName: "symbols", hideDefaultActions: true },
+  { label: "Value", fieldName: "value", hideDefaultActions: true }
+];
+
+export default class SimplePagination extends LightningElement {
+  @api tabledata = [];
+
+  @track page = 1;
+  @track pages = [];
+  perpage = 5;
+  set_size = 5;
+
+  columns = columns;
+
+  renderedCallback() {
+    this.renderButtons();
+  }
+  renderButtons = () => {
+    this.template.querySelectorAll("button").forEach((but) => {
+      // but.style.backgroundColor = this.page === parseInt(but.dataset.id, 10) ? 'lightgrey' : 'white';
+      but.className =
+        "slds-button " +
+        (this.page === parseInt(but.dataset.id, 10)
+          ? "slds-button_brand"
+          : "slds-button_neutral");
+    });
+  };
+  get pagesList() {
+    let mid = Math.floor(this.set_size / 2) + 1;
+    if (this.page > mid) {
+      return this.pages.slice(this.page - mid, this.page + mid - 1);
+    }
+    return this.pages.slice(0, this.set_size);
+  }
+
+  connectedCallback() {
+    this.setPages(this.tabledata);
+  }
+
+  pageData = () => {
+    let page = this.page;
+    let perpage = this.perpage;
+    let startIndex = page * perpage - perpage;
+    let endIndex = page * perpage;
+    return this.tabledata.slice(startIndex, endIndex);
+  };
+
+  setPages = (data) => {
+    let numberOfPages = Math.ceil(data.length / this.perpage);
+    for (let index = 1; index <= numberOfPages; index++) {
+      this.pages.push(index);
+    }
+  };
+
+  get hasPrev() {
+    return this.page > 1;
+  }
+
+  get hasNext() {
+    return this.page < this.pages.length;
+  }
+
+  onNext = () => {
+    ++this.page;
+  };
+
+  onPrev = () => {
+    --this.page;
+  };
+
+  onPageClick = (e) => {
+    this.page = parseInt(e.target.dataset.id, 10);
+  };
+
+  get currentPageData() {
+    return this.pageData();
+  }
+}
